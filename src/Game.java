39e009637e75467c3defdb77c8c4976417c36d45
Andrey Belianin <belyaninwork@gmail.com>
2018-09-14 15:41:13 +0500

    Word catcher added

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,40 @@
 import java.util.Scanner;
+
 public class Game {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		Room currentRoom;
-		
-		while (true)
-		{
-			String request = input.nextLine().toLowerCase().replaceAll("\\s","");
-			if (currentRoom.KeyWords.containsKey(request))
-				currentRoom.KeyWords.get(request);
-		}
+	private static Scanner input = new Scanner(System.in);
 
+	public static void main(String[] args) {
+
+		Room currentRoom = new Room();
+
+		while (true) {
+			String[] requestWords = GetWords();
+
+			Action action = null;
+			for (String word : requestWords) {
+				if (currentRoom.KeyWords.containsKey(word)) {
+					Action act = currentRoom.KeyWords.get(word);
+					if (action == null || action == act)
+						action = act;
+					else {
+						System.out.println("Выберите что-то одно");
+						action = null;
+						break;
+					}
+				}
+			}
+			if (action != null) {
+				action.Act();
+				// не обязательно обnullять, но на всякий случай
+				action = null;
+			}
+		}
+	}
+
+	public static String[] GetWords() {
+		// В этом методе потом сможем сменить ввод с консольного, на Алису
+		return input.nextLine().toLowerCase().split(" ");// .replaceAll("\\s","");
 	}
 
 }
