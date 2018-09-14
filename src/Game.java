42e76b34b3965bb78331cdcbe1e62f04a5fd4b35
Andrey Belianin <belyaninwork@gmail.com>
2018-09-14 17:22:17 +0500

    Finally stable mainloop

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -6,55 +5,60 @@ import java.util.Scanner;
 public class Game {
 
 	private static Scanner input = new Scanner(System.in);
+	private static Map<String, Action> commonCommands = new HashMap<String, Action>();
+	private static Level level = new Level();
+	private static Room currentRoom = level.GetStartRoom();
 
 	public static void main(String[] args) {
-
-		Level level = new Level();
-		Room currentRoom = level.GetStartRoom();
-		Map<String, Action> commonCommands = new HashMap<String, Action>();
 
 		while (true) {
 			System.out.println(currentRoom.getDescription());
 			String[] requestWords = getWords();
-			// ДУБЛЮРУЮЩИЙСЯ КОД!!!! АЛАРМ
-			Action action = null;
+			Action action = getAction(requestWords);
 
-			for (String word : requestWords)
-				if (commonCommands.containsKey(word)) {
-					Action act = commonCommands.get(word);
-					if (action == null || action == act)
-						action = act;
-					else {
-						action = null;
-						break;
-					}
-				}
 			if (action != null) {
 				action.Act();
-				action = null;
-			} else {
-
-				for (String word : requestWords) {
-					if (currentRoom.KeyWords.containsKey(word)) {
-						Action act = currentRoom.KeyWords.get(word);
-						if (action == null || action == act)
-							action = act;
-						else {
-							System.out.println("You must chose one option!");
-							action = null;
-							break;
-						}
-					}
-				}
-				if (action != null) {
-					action.Act();
-					// Actually there is no need to null it, but i do it just in case
-					action = null;
-				}
 			}
 		}
 	}
 
+	public static Action getAction(String[] words) {
+		Action action = null;
+
+		for (String word : words) {
+			// проверяем если ли слова в общих командах
+			if (commonCommands.containsKey(word)) {
+				Action act = commonCommands.get(word);
+				if (action == null || action == act)
+					action = act;
+				else {
+					// если слов несколько сообщаем об этом и ничего не делаем
+					System.out.println("Вы должны выбрать только одно действие!");
+					action = null;
+					break;
+				}
+			}
+		}
+		//если нашли слово в общих командах, то отправляем команду
+		if (action != null)
+			return action;
+
+		// иначе проверяем уже среди команд комнаты
+		for (String word : words) {
+			if (currentRoom.KeyWords.containsKey(word)) {
+				Action act = currentRoom.KeyWords.get(word);
+				if (action == null || action == act)
+					action = act;
+				else {
+					System.out.println("Вы должны выбрать только одно действие!");
+					action = null;
+					break;
+				}
+			}
+		}
+		return action;
+	}
+	
 	public static String[] getWords() {
 		// Later we can change our input to Alice or whatever we want
 		return input.nextLine().toLowerCase().split(" ");// .replaceAll("\\s","");
