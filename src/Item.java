548ba595a5dfd907a076e32a69bfd867e0e17b6a
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-09-14 16:02:14 +0500

    Item & Player

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,8 @@
 
 public class Item {
+    private int Price;
 
+    public Item(int price){
+        Price = price;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class Player {
+    private int Health;
+    private int MaxHealth = 100;
+    private int Money;
+    private Item ActiveItem;
+    private ArrayList<Item> ItemList;
 
+    public Player(){
+        Health = MaxHealth;
+        Money = 0;
+    }
 }
