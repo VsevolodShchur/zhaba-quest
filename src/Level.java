c8a07bb1800b8be2c3145a25a29d50debc242f07
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-09-14 16:39:22 +0500

    левель

--- src/Level.java
+++ src/Level.java
@@ -5,9 +5,14 @@ public class Level {
     private ArrayList<Room> roomList;
     public Level (){
         roomList = new ArrayList<>();
+        roomList.add(new Room());
     }
     public Level (ArrayList<Room> roomList1){
         roomList = roomList1;
+    }
+
+    public Level (String fileName){
+        
     }
 
     public Room GetStartRoom(){
